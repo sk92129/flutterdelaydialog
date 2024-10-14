6b5ca9b8c8f4e3826231b97dc251d8e637a35db2
Sean Kang <skang@geo-blue.com>
2024-10-14 11:21:39 -0700

    initial

---
+++ android/app/src/main/kotlin/com/example/delayscreen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.delayscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+ @override
+  void initState() {
+
+    super.initState();
+    Future.delayed( const Duration(seconds: 2), () {
+      showDialog(context: context,
+          builder: (BuildContext context) {
+            return  AlertDialog(
+              title: const Text("hello"),
+              content: const Text("world"),
+              actions: <Widget> [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(context),
+                  child: const Text("OK"),
+                ),
+              ],
+            );
+          });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: const Text("hello"),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'foo',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
